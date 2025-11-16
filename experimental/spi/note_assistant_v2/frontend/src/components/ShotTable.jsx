4ff79b2ab47d814fa1e193c2fff38527edf28dc9
Cottalango Leon <loorthu@imageworks.com>
2025-11-15 18:27:29 -0800

    Add extra path to pin icon SVG in ShotTable
    An additional <path> element was added to the pin icon SVG in the ShotTable component, enhancing the icon's appearance.
    
    Signed-off-by: Cottalango Leon <loorthu@imageworks.com>

--- experimental/spi/note_assistant_v2/frontend/src/components/ShotTable.jsx
+++ experimental/spi/note_assistant_v2/frontend/src/components/ShotTable.jsx
@@ -295,9 +295,10 @@ function ShotTable({
                     aria-label="Pin"
                     onClick={() => setPinnedIndex(isPinned ? null : idx)}
                   >
-                    {/* Pin icon from Iconoir (https://iconoir.com/) */}
+                  {/* Pin icon from Iconoir (https://iconoir.com/) */}
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M9.5 14.5L3 21" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M5.00007 9.48528L14.1925 18.6777L15.8895 16.9806L15.4974 13.1944L21.0065 8.5211L15.1568 2.67141L10.4834 8.18034L6.69713 7.78823L5.00007 9.48528Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   </button>
                 </td>
